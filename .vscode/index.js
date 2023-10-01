103d2c5581f4eb39b0c65b234ba3f6fea1564019
pqnpetit <pqnpetit@gmail.com>
2023-10-01 20:48:51 -0300

    Beta

--- .vscode/index.js
+++ .vscode/index.js
@@ -1,3 +1,19 @@
 
-const buttons = document.querySelectorAll('.button')
+const buttons = document.querySelectorAll(".button");
+const characters = document.querySelectorAll(".character");
 
+
+buttons.forEach((button, index) => {
+    button.addEventListener("click", () => {
+        
+        const buttonSelected = document.querySelector(".button.selected");
+        buttonSelected.classList.remove("selected");
+        button.classList.add("selected");
+
+        
+        const characterSelected = document.querySelector(".character.selected");
+        characterSelected.classList.remove("selected");
+
+        characters[index].classList.add("selected");
+    });
+});
